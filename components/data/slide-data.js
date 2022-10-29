4af4a7a2e061f11ba6954769617ffa3d9b013d66
Ingpawat <Ingpawat.k@gmail.com>
2022-10-29 19:31:13 +0700

    add data

--- components/data/slide-data.js
+++ components/data/slide-data.js
@@ -1,16 +1,28 @@
 export const slideData = [
     {
-        img:'https://collectionapi.metmuseum.org/api/collection/v1/iiif/755191/1756563/main-image',
-        heading:'"Damocles was given power, but had a sword above his head. Once Damocles realized the sword, he lost his appetite for the power It portrayed the constant fear that the powerful Dionysius lived with."',
-        desc:'The president of the federal diet or the double sword of Damocles, Le Charivari, June 22, 1866'
+        img: 'https://collectionapi.metmuseum.org/api/collection/v1/iiif/755191/1756563/main-image',
+        heading: '"Damocles was given power, but had a sword above his head. Once Damocles realized the sword, he lost his appetite for the power It portrayed the constant fear that the powerful Dionysius lived with."',
+        desc: 'The president of the federal diet or the double sword of Damocles, Le Charivari, June 22, 1866'
     },
+
     {
-        img:'https://res.cloudinary.com/dlyijmtfx/image/upload/v1667028638/project-pics/IMG_5161_iuiiab.jpg',
-        heading:'"Meawwwwwoww"',
-        desc:'- Moo Kata (My Cat)'
+        img: 'https://imgix.bustle.com/wmag/2016/09/06/57cf1630449b3b0baf16dcb4_1316.w.MS_.royals.image3_.jpg?w=414&h=499&fit=crop&crop=faces&auto=format%2Ccompress',
+        heading: '"I need a room full of mirrors so I can be surrounded by winners."',
+        desc: '- Ye'
     },
+
     
-   
+    {
+        img: 'https://static01.nyt.com/images/2017/06/12/fashion/12ABLOH-INYT-WEB1-sub/12ABLOH-INYT-WEB1-sub-superJumbo.jpg',
+        heading: `"Life is so short you can't not waste even a day subscribing to what someone thinks you can do versus knowing what you can do"`,
+        desc: '– Virgil Abloh'
+    },
+
+    {
+        img: 'https://res.cloudinary.com/dlyijmtfx/image/upload/v1667028638/project-pics/IMG_5161_iuiiab.jpg',
+        heading: '"Meawwwwwoww"',
+        desc: '- Moo Kata (My Cat)'
+    },
 
 ]
 
