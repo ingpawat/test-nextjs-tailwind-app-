f7f653a6a3d48491f660f88c07ba4698f0ba0791
ingpawat / Y <ingpawat.k@gmail.com>
2022-11-05 15:01:27 +0700

    fix: failed: found page without a React Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css'
-import styles from '../styles/slide.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/xxx.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const xxx = () => {
-  return (
-    <div>xxx</div>
-  )
-}
-
